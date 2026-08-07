50919029cce41824b833d2cb69fc3cc7f0fff1d8
Moritz Helm <mohelm84@gmail.com>
2026-08-07 22:53:10 +0200

    Add some more constants and a construct.

--- src/qte/constants.py
+++ src/qte/constants.py
@@ -1,1 +1,11 @@
-QUARTILES = [0.25, 0.5, 0.75]
+import numpy as np
+from numpy.typing import NDArray
+
+
+def _make_q(n: int) -> NDArray:
+    return np.linspace(0, 1, n + 1)[1:-1]
+
+
+QUARTILES = _make_q(4)
+QUANTILES = _make_q(5)
+DECILES = _make_q(10)
