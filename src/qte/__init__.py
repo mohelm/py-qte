a0959f8b30e4d482cbe80a3e64c03ccb33199683
Moritz Helm <mohelm84@gmail.com>
2026-09-26 22:29:30 +0200

    Built site for gh-pages

--- src/qte/__init__.py
+++
@@ -1,8 +0,0 @@
-"""Quantile Treatment Effects (qte)."""
-
-from importlib.metadata import PackageNotFoundError, version
-
-try:
-    __version__ = version("py-qte")
-except PackageNotFoundError:  # pragma: no cover - uninstalled source tree
-    __version__ = "0.0.0.dev0"
